556e24555e6ffdf26d4b2751bf8a473e26006201
ryosuke <ryskosn@gmail.com>
2015-03-28 13:24:07 +0900

    add mode-line settings

--- init.el
+++ init.el
@@ -27,6 +27,17 @@
 ;; scratch の初期メッセージをオフにする
 (setq initial-scratch-message "")
 
+;; メニューバーを非表示
+(menu-bar-mode -1)
+
+;;;; iTerm2 上で使う場合は不要 --------------
+;; ;; ツールバー非表示
+;; (tool-bar-mode -1)
+
+;; ;; スクロールバー非表示
+;; (set-scroll-bar-mode nil)
+;;;; ----------------------------------------
+
 ;; バックアップファイルを作らない
 (setq make-backup-files nil)
 
@@ -39,22 +50,6 @@
 ;; 標準の祝日を利用しない
 (setq calendar-holidays nil)
 
-;; メニューバーを非表示
-(menu-bar-mode -1)
-
-;; モードラインに時間を表示する
-(display-time)
-
-;; カーソル点滅
-(blink-cursor-mode 1)
-
-;; バッファ再読み込み
-(global-auto-revert-mode 1)
-
-;; 行番号の表示
-(global-linum-mode)
-(setq linum-format "%4d ")
-
 ;; yes or no を y or n にする
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -64,6 +59,20 @@
 
 ;; 補完可能なものを随時表示（少しうるさい）
 (icomplete-mode 1)
+
+;; カーソル点滅
+(blink-cursor-mode 1)
+
+;; バッファ再読み込み
+(global-auto-revert-mode 1)
+
+;; タイトルバーにファイルのフルパス表示
+(setq frame-title-format
+      (format "%%f - Emacs@%s" (system-name)))
+
+;; 行番号の表示
+(global-linum-mode)
+(setq linum-format "%4d ")
 
 ;; 現在行をハイライト
 ;;; http://shibayu36.hatenablog.com/entry/2012/12/29/001418
@@ -84,14 +93,40 @@
 (show-paren-mode t)   ; 有効化
 (setq show-paren-delay 0)   ; 表示までの秒数
 
-;; Command を Meta キーとする
-;; http://qiita.com/hayamiz/items/0f0b7a012ec730351678
-(setq ns-command-modifier (quote meta))
-(setq ns-alternate-modifier (quote super))
-
 ;; スペルチェック
 (setq-default flyspell-mode t)
 (setq ispell-dictionary "american")
+
+;; 最近使ったファイルの表示数
+(setq recentf-max-menu-items 10)
+
+;; 最近開いたファイルの保存数を増やす
+(setq recentf-max-saved-items 3000)
+
+;; ミニバッファの履歴を保存する
+(savehist-mode 1)
+
+;; ミニバッファの履歴の保存数を増やす
+(setq history-length 3000)
+
+;; 行間
+(setq-default line-spacing 0)
+
+;; C-Ret で矩形選択
+;; 詳しいキーバインド操作
+;; http://dev.ariel-networks.com/articles/emacs/part5/
+(cua-mode t)
+(setq cua-enable-cua-keys nil)
+
+;; 行末の空白を強調表示
+(setq-default show-trailing-whitespace t)
+(set-face-background 'trailing-whitespace "#b14770")
+
+;; タブをスペースで扱う
+(setq-default indent-tabs-mode nil)
+
+;; タブ幅
+(custom-set-variables '(tab-width 4))
 
 ;;; diredを便利にする
 (require 'dired-x)
@@ -103,6 +138,78 @@
 ;; バッファ名を変更する
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+
+;; -------------------------------------------------------------------------
+;; @ mode-line
+
+;; モードラインに時間を表示する
+(display-time)
+
+;; モードラインに行番号表示
+(line-number-mode t)
+
+;; モードラインに列番号表示
+(column-number-mode t)
+
+;; モードラインの割合表示を総行数表示
+(defvar my-lines-page-mode t)
+(defvar my-mode-line-format)
+
+(when my-lines-page-mode
+  (setq my-mode-line-format "%d")
+  (if size-indication-mode
+      (setq my-mode-line-format (concat my-mode-line-format " of %%I")))
+  (cond ((and (eq line-number-mode t) (eq column-number-mode t))
+         (setq my-mode-line-format (concat my-mode-line-format " (%%l,%%c)")))
+        ((eq line-number-mode t)
+         (setq my-mode-line-format (concat my-mode-line-format " L%%l")))
+        ((eq column-number-mode t)
+         (setq my-mode-line-format (concat my-mode-line-format " C%%c"))))
+
+  (setq mode-line-position
+        '(:eval (format my-mode-line-format
+        (count-lines (point-max) (point-min))))))
+
+;; powerline
+;; http://shibayu36.hatenablog.com/entry/2014/02/11/160945
+;; (require 'powerline)
+;; (powerline-default-theme)
+
+
+;; -------------------------------------------------------------------------
+;; @ Mac OS X
+
+;; iTerm2 で使うのでフォントの設定などは iTerm2 側で行う
+
+;; Command を Meta キーとする
+;; http://qiita.com/hayamiz/items/0f0b7a012ec730351678
+(setq ns-command-modifier (quote meta))
+(setq ns-alternate-modifier (quote super))
+
+;; IME
+;; http://b.mgrace.info/?p=1128
+(setq default-input-method "MacOSX")
+;; ;; emacs 起動時は英数モードから始める  
+;; (add-hook 'after-init-hook 'mac-change-language-to-us)
+;; minibuffer 内は英数モードにする  
+;; (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
+;; ;; backslash を先
+;; (mac-translate-from-yen-to-backslash)
+
+;; http://qiita.com/catatsuy/items/886f1e0632c0b2760fb4
+;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" 'title "あ")
+
+;; OS X とクリップボードを同期する
+(defun copy-from-osx ()
+ (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+ (let ((process-connection-type nil))
+     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+       (process-send-string proc text)
+       (process-send-eof proc))))
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
 
 
 ;; -------------------------------------------------------------------------
@@ -127,12 +234,6 @@
 ;; -------------------------------------------------------------------------
 ;; @ Package, Cask, auto-install
 
-;; Cask
-;; http://cask.readthedocs.org/en/latest/guide/usage.html
-;; http://d.hatena.ne.jp/syohex/20140424/1398310931
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-
 ;; load-path の設定
 (setq load-path
      (append
@@ -140,23 +241,36 @@
 	(expand-file-name "~/.emacs.d/elisp/"))
        load-path))
 
-;; auto-install で Elisp をインストール
-(require 'auto-install)
-(auto-install-update-emacswiki-package-name t)
-(auto-install-compatibility-setup)
+;; packages
+(require 'package)
+;; 使い方
+;; http://emacs-jp.github.io/packages/package-management/package-el.html
 
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; Marmaladeを追加
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; 初期化
+(package-initialize)
 
-;; 透明度を変更するコマンド M-x set-alpha
-;; http://qiita.com/marcy@github/items/ba0d018a03381a964f24
-(defun set-alpha (alpha-num)
-  "set frame parameter 'alpha"
-  (interactive "nAlpha: ")
-  (set-frame-parameter nil 'alpha (cons alpha-num '(90))))
+;; Cask
+;; http://cask.readthedocs.org/en/latest/guide/usage.html
+;; http://d.hatena.ne.jp/syohex/20140424/1398310931
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
 
-
-;; undo-treeモードの設定
-(when (require 'undo-tree nil t)
-  (global-undo-tree-mode))
+;; auto-install
+;; http://www.emacswiki.org/emacs/auto-install.el
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (auto-install-compatibility-setup))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 
 ;; -------------------------------------------------------------------------
@@ -170,7 +284,6 @@
 (anything-read-string-mode 1)
 (require 'anything-show-completion)
 (global-set-key "\C-x\C-b" 'anything-filelist+)
-
 
 ;; anything でファイルを開く
 ;; http://shibayu36.hatenablog.com/entry/2012/12/29/001418
@@ -196,7 +309,6 @@
 ;; M-x anything (anything) を C-o に割り当てる
 (global-set-key (kbd "C-o") 'anything)
 
-
 ;; C-x o (other-window) の代わり
 ;; (global-set-key (kbd "C-t") 'other-window)
 ;; http://d.hatena.ne.jp/rubikitch/20100210/emacs
@@ -206,30 +318,6 @@
     (split-window-horizontally))
   (other-window 1))
 (global-set-key (kbd "C-t") 'other-window-or-split)
-
-
-;; -------------------------------------------------------------------------
-;; @ popwin
-
-;; http://valvallow.blogspot.jp/2011/03/emacs-popwinel.html
-
-(require 'popwin)
-(defvar popwin:special-display-config-backup popwin:special-display-config)
-(setq display-buffer-function 'popwin:display-buffer)
-(setq popwin:special-display-config
-      (append '(
-		("*Remember*" :stick t)
-		("*Org Agenda*" :height 0.5)
-		("anything" :regexp t :height 0.5)
-		("*Backtrace*")
-		("*quickrun*" :height 0.4)
-		("magit" :regexp t :height 0.5)
-		("*Dired*" :height 0.5)
-		("COMMIT_EDITMSG")
-                ("*sdic*" :noselect)
-		)
-              popwin:special-display-config))
-(define-key global-map (kbd "C-x p") 'popwin:display-last-buffer)
 
 
 ;; -------------------------------------------------------------------------
@@ -308,70 +396,32 @@
 (global-auto-complete-mode t)
 
 
-;; key-chord.el をインストール
-;; ;; M-x install-elisp-from-emacswiki RET  key-chord.el
-;; (require 'key-chord)
-;; (key-chord-mode 1)
-;; ;; jk 同時押しで M-x org-remember を実行
-;; (key-chord-define-global "jk" 'org-remember)
+;; -------------------------------------------------------------------------
+;; @ yasnippet
 
+;; http://konbu13.hatenablog.com/entry/2014/01/12/113300
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/mySnippets"
+	"~/.emacs.d/snippets"
+	))
+(yas-global-mode 1)
 
-;; open-junk-file
-;; http://d.hatena.ne.jp/rubikitch/20080923/1222104034
-(require 'open-junk-file)
-(setq open-junk-file-format "~/Dropbox/junk/%Y%m%d-%H%M%S." )
-(global-set-key (kbd "C-x j") 'open-junk-file)
+;; download snippets from GitHub
+;; ~/.emacs.d/
+;; % git clone https://github.com/AndreaCrotti/yasnippet-snippets.git snippets
 
-;; OS X とクリップボードを同期する
-(defun copy-from-osx ()
- (shell-command-to-string "pbpaste"))
-
-(defun paste-to-osx (text &optional push)
- (let ((process-connection-type nil))
-     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-       (process-send-string proc text)
-       (process-send-eof proc))))
-
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
-
-
-;; 分割したバッファを入れ替える
-;; http://www.bookshelf.jp/soft/meadow_30.html#SEC403
-(defun swap-screen()
-  "Swap two screen,leaving cursor at current window."
-  (interactive)
-  (let ((thiswin (selected-window))
-        (nextbuf (window-buffer (next-window))))
-    (set-window-buffer (next-window) (window-buffer))
-    (set-window-buffer thiswin nextbuf)))
-(defun swap-screen-with-cursor()
-  "Swap two screen,with cursor in same buffer."
-  (interactive)
-  (let ((thiswin (selected-window))
-        (thisbuf (window-buffer)))
-    (other-window 1)
-    (set-window-buffer thiswin (window-buffer))
-    (set-window-buffer (selected-window) thisbuf)))
-(global-set-key [f2] 'swap-screen) ; F2 で入れ替え
-(global-set-key [S-f2] 'swap-screen-with-cursor) ; shift+F2 でカーソルごと入れ替え
-
-
-;; 現在時刻の挿入
-;; http://d.hatena.ne.jp/CortYuming/20101125/p1
-(defun my-insert-date ()
-  (interactive)
-  (insert (concat
-	   "" (format-time-string "%Y-%m-%d(%a) %H:%M:%S "))))
-
-;; ;; df 同時押しで M-x my-insert-date を実行
-;; (key-chord-define-global "df" 'my-insert-date)
+;; insert snippet
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; new snippet
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; view, edit snippet
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
 
 ;; -------------------------------------------------------------------------
 ;; @ autoinsert
 
-;; autoinsert
 ;; http://ymotongpoo.hatenablog.com/entry/2012/12/02/190248
 (require 'autoinsert)
 
@@ -405,30 +455,52 @@
 (add-hook 'find-file-not-found-hooks 'auto-insert)
 
 
-;; fly-check
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;; -------------------------------------------------------------------------
+;; @ popwin
+
+;; http://valvallow.blogspot.jp/2011/03/emacs-popwinel.html
+
+(require 'popwin)
+(defvar popwin:special-display-config-backup popwin:special-display-config)
+(setq display-buffer-function 'popwin:display-buffer)
+(setq popwin:special-display-config
+      (append '(
+		("*Remember*" :stick t)
+		("*Org Agenda*" :height 0.5)
+		("anything" :regexp t :height 0.5)
+		("*Backtrace*")
+		("*quickrun*" :height 0.4)
+		("magit" :regexp t :height 0.5)
+		("*Dired*" :height 0.5)
+		("COMMIT_EDITMSG")
+                ("*sdic*" :noselect)
+		)
+              popwin:special-display-config))
+(define-key global-map (kbd "C-x p") 'popwin:display-last-buffer)
 
 
+;; -------------------------------------------------------------------------
+;; @ 便利関数いろいろ
 
-;; powerline
-;; http://shibayu36.hatenablog.com/entry/2014/02/11/160945
-;; (require 'powerline)
-;; (powerline-default-theme)
-
-
-;;;; IME
-;;; http://b.mgrace.info/?p=1128
-(setq default-input-method "MacOSX")
-;; ;; emacs 起動時は英数モードから始める  
-;; (add-hook 'after-init-hook 'mac-change-language-to-us)
-;; minibuffer 内は英数モードにする  
-;; (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
-;; ;; backslash を先
-;; (mac-translate-from-yen-to-backslash)
-
-;; http://qiita.com/catatsuy/items/886f1e0632c0b2760fb4
-;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" 'title "あ")
-
+;; 分割したバッファを入れ替える
+;; http://www.bookshelf.jp/soft/meadow_30.html#SEC403
+(defun swap-screen()
+  "Swap two screen,leaving cursor at current window."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (nextbuf (window-buffer (next-window))))
+    (set-window-buffer (next-window) (window-buffer))
+    (set-window-buffer thiswin nextbuf)))
+(defun swap-screen-with-cursor()
+  "Swap two screen,with cursor in same buffer."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (thisbuf (window-buffer)))
+    (other-window 1)
+    (set-window-buffer thiswin (window-buffer))
+    (set-window-buffer (selected-window) thisbuf)))
+(global-set-key [f2] 'swap-screen) ; F2 で入れ替え
+(global-set-key [S-f2] 'swap-screen-with-cursor) ; shift + F2 でカーソルごと入れ替え
 
 ;; window resize
 ;;; http://d.hatena.ne.jp/khiker/20100119/window_resize
@@ -469,16 +541,25 @@
                (throw 'end-flag t)))))))
 (global-set-key "\C-c\C-r" 'my-window-resizer)
 
+;; 透明度を変更するコマンド M-x set-alpha
+;; http://qiita.com/marcy@github/items/ba0d018a03381a964f24
+(defun set-alpha (alpha-num)
+  "set frame parameter 'alpha"
+  (interactive "nAlpha: ")
+  (set-frame-parameter nil 'alpha (cons alpha-num '(90))))
 
-;; visual-regexp
-;;; https://github.com/benma/visual-regexp.el
-(require 'visual-regexp)
-(global-set-key "\M-%" 'vr/query-replace)
+;; 現在時刻の挿入
+;; http://d.hatena.ne.jp/CortYuming/20101125/p1
+(defun my-insert-date ()
+  (interactive)
+  (insert (concat
+	   "" (format-time-string "%Y-%m-%d(%a) %H:%M:%S "))))
 
 
 ;; -------------------------------------------------------------------------
-;; @ migemo
+;; @ 検索置換
 
+;; migemo
 ;; http://weblog.ymt2.net/blog/html/2013/08/23/install_migemo_to_emacs_24_3_1.html
 ;; http://rubikitch.com/2014/08/20/migemo/
 (require 'migemo)
@@ -491,9 +572,14 @@
 (load-library "migemo")
 (migemo-init)
 
+;; visual-regexp
+;; https://github.com/benma/visual-regexp.el
+(require 'visual-regexp)
+(global-set-key "\M-%" 'vr/query-replace)
+
 
 ;; -------------------------------------------------------------------------
-;; @ Languages
+;; @ Languages, programming
 
 ;; jedi Python
 ;; http://d.hatena.ne.jp/CortYuming/20130415/p1
@@ -534,14 +620,7 @@
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
 
-
-;;;; flex-autopair.el
-;; http://d.hatena.ne.jp/uk-ar/20120401/1333282805
-(require 'flex-autopair)
-(flex-autopair-mode 1)
-
-
-;;;; quickrun
+;; quickrun
 ;; https://github.com/syohex/emacs-quickrun
 ;; http://yunojy.github.io/blog/2013/03/17/emacs-de-quickrun-or-quickrun-region/
 (require 'quickrun)
@@ -552,16 +631,29 @@
   (quickrun)))
 (global-set-key (kbd "<f5>") 'quickrun-sc)
 
+;; fly-check
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
 
 ;; ------------------------------------------------------------------------
-;; @ Git
+;; @ 便利 packages いろいろ
 
-;; magit
-;; http://qiita.com/takc923/items/c7a11ff30caedc4c5ba7
-(require 'magit)
+;; open-junk-file
+;; http://d.hatena.ne.jp/rubikitch/20080923/1222104034
+(require 'open-junk-file)
+(setq open-junk-file-format "~/Dropbox/junk/%Y%m%d-%H%M%S." )
+(global-set-key (kbd "C-x j") 'open-junk-file)
 
+;; key-chord.el
+;; ;; M-x install-elisp-from-emacswiki RET  key-chord.el
+;; (require 'key-chord)
+;; (key-chord-mode 1)
+;; ;; jk 同時押しで M-x org-remember を実行
+;; (key-chord-define-global "jk" 'org-remember)
+;; ;; df 同時押しで M-x my-insert-date を実行
+;; (key-chord-define-global "df" 'my-insert-date)
 
-;;;; text-adjust.el
+;; text-adjust.el
 ;; http://d.hatena.ne.jp/rubikitch/20090220/text_adjust
 ;; http://rubikitch.com/f/text-adjust.el
 ;; http://rubikitch.com/f/mell.el
@@ -573,28 +665,22 @@
 (defalias 'spacer 'text-adjust-space-buffer)
 (add-hook 'before-save-hook 'text-adjust-space-before-save-if-needed)
 
+;; flex-autopair.el
+;; http://d.hatena.ne.jp/uk-ar/20120401/1333282805
+(require 'flex-autopair)
+(flex-autopair-mode 1)
 
-;; -------------------------------------------------------------------------
-;; @ yasnippet
+;; undo-tree
+(when (require 'undo-tree nil t)
+  (global-undo-tree-mode))
 
-;; http://konbu13.hatenablog.com/entry/2014/01/12/113300
-(require 'yasnippet)
-(setq yas-snippet-dirs
-      '("~/.emacs.d/mySnippets"
-	"~/.emacs.d/snippets"
-	))
-(yas-global-mode 1)
 
-;; download snippets from GitHub
-;; ~/.emacs.d/
-;; % git clone https://github.com/AndreaCrotti/yasnippet-snippets.git snippets
+;; ------------------------------------------------------------------------
+;; @ Git
 
-;; insert snippet
-(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
-;; new snippet
-(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
-;; view, edit snippet
-(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+;; magit
+;; http://qiita.com/takc923/items/c7a11ff30caedc4c5ba7
+(require 'magit)
 
 
 ;;; init.el ends here
