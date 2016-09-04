71032d52db503a949cccbaa0c99cdaf4dd27423d
ryosuke <ryskosn@gmail.com>
2016-09-04 20:41:04 +0900

    comment out some line

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ ln -s ~/dotfiles/.slate.js ~/.slate.js
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
 ln -s ~/dotfiles/Cask ~/.emacs.d/Cask
+# ln -s ~/dotfiles/setting.json ~/.vscode/setting.json
 
 echo set symbolic link done!
