87794a7812c5f943a20791b8b818d27838ef7475
ryosuke <ryskosn@gmail.com>
2016-12-07 13:26:47 +0900

    add calendar config about japanese-holidays

--- init.el
+++ init.el
@@ -48,12 +48,6 @@
 ;; オートセーブを無効にする
 (setq auto-save-default nil)
 
-;; Calendar 月曜始まりにする
-(setq calendar-week-start-day 1)
-
-;; 標準の祝日を利用しない
-(setq calendar-holidays nil)
-
 ;; yes or no を y or n にする
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -173,6 +167,35 @@
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 ;; diffのバッファを上下ではなく左右に並べる
 (setq ediff-split-window-function 'split-window-horizontally)
+
+
+;; -------------------------------------------------------------------------
+;; @ calendar
+
+;; Calendar 月曜始まりにする
+(setq calendar-week-start-day 1)
+
+;; 標準の祝日を利用しない
+(setq calendar-holidays nil)
+
+;; http://emacs.rubikitch.com/japanese-holidays/
+
+(with-eval-after-load "calendar"
+  (require 'japanese-holidays)
+  (setq calendar-holidays ; 他の国の祝日も表示させたい場合は適当に調整
+        (append japanese-holidays holiday-local-holidays holiday-other-holidays))
+  (setq calendar-mark-holidays-flag t)    ; 祝日をカレンダーに表示
+  ;; 土曜日・日曜日を祝日として表示する場合、以下の設定を追加します。
+  ;; 変数はデフォルトで設定済み
+  (setq japanese-holiday-weekend '(0 6)    ; 土日を祝日として表示
+        japanese-holiday-weekend-marker    ; 土曜日を水色で表示
+        '(holiday nil nil nil nil nil japanese-holiday-saturday))
+  (add-hook 'calendar-today-visible-hook 'japanese-holiday-mark-weekend)
+  (add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend)
+  ;; “きょう”をマークするには以下の設定を追加します。
+  (add-hook 'calendar-today-visible-hook 'calendar-mark-today)
+  ;; org-agendaで祝日を表示する
+  (setq org-agenda-include-diary t))
 
 
 ;; -------------------------------------------------------------------------
