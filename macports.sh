cf7ea2ccfa2f592bee5173ee9651a35d21f28300
ryosuke <ryskosn@gmail.com>
2017-10-15 03:15:14 +0900

    initial commit

---
+++ macports.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+sudo port selfupdate
+sudo port install fish
+sudo port install emacs
+sudo port install git
+sudo port install findutils
+sudo port install coreutils
+sudo port install go
+sudo port install python36
+sudo port install py36-readline
+sudo port install opam
+sudo port install nkf
+sudo port install tree
+sudo port install wget
+sudo port install tmux
+sudo port install tig
+sudo port install chromedriver
