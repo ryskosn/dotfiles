18d076f15fa535d8c8741e31273de78e01c950e0
ryosuke <ryskosn@gmail.com>
2018-01-01 14:45:28 +0900

    add haskell config

--- init.el
+++ init.el
@@ -974,4 +974,34 @@
 ;; #> (+ 1 3)
 (global-set-key (kbd "<f9>") 'wand:execute-current-line)
 
+
+;;; --------------------------------------------------------
+;; Haskell
+;; http://blog.515hikaru.net/entry/2016/09/12/021206
+
+(autoload 'haskell-mode "haskell-mode" nil t)
+(autoload 'haskell-cabal "haskell-cabal" nil t)
+
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal$" . haskell-cabal-mode))
+
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+
+(defun my-haskell-mode-hook ()
+    (interactive)
+    ;; インデント
+    (turn-on-haskell-indentation)
+    (turn-on-haskell-doc-mode)
+    (font-lock-mode)
+    (imenu-add-menubar-index)
+    ;; GHCi のコマンドを設定
+    (setq haskell-program-name "/usr/bin/stack ghci") ;; stack の場合
+    (inf-haskell-mode)
+    ;; ghc-mod を使えるように
+    (ghc-init)
+    ;; flycheck を起動
+    (flycheck-mode))
+(add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
 ;;; init.el ends here
